7c459bf0e2ffb3ac6eaad7217c85e7ab1f07c3ed
sleepymarcy <martyna.sowinska@hotmail.com>
2025-12-05 23:23:41 +0100

    feat: :sparkles: add WallBundleFactory constructor

--- src/main.rs
+++ src/main.rs
@@ -53,29 +53,22 @@ fn spawn_circle(
 
 fn spawn_walls(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut color_materials: ResMut<Assets<ColorMaterial>>,
+    meshes: ResMut<Assets<Mesh>>,
+    color_materials: ResMut<Assets<ColorMaterial>>,
 ) {
     const WALL_THICKNESS: f32 = 20.0;
     const WINDOW_WIDTH: f32 = 1280.0;
     const WINDOW_HEIGHT: f32 = 720.0;
+    const WALL_COLOR: Color = Color::srgb(0., 0., 0.);
 
-    // TODO: Move to a factory constructor
-    let mesh_handle = meshes.add(Rectangle {
-        half_size: Vec2 {
-            x: WALL_THICKNESS / 2.0,
-            y: WALL_THICKNESS / 2.0,
-        },
-    });
-    let material_handle = color_materials.add(Color::BLACK);
-
-    let mut factory = WallBundleFactory {
-        window_width: WINDOW_WIDTH,
-        window_height: WINDOW_HEIGHT,
-        wall_thickness: WALL_THICKNESS,
-        wall_mesh: mesh_handle,
-        wall_material: material_handle,
-    };
+    let mut factory = WallBundleFactory::new(
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WALL_THICKNESS,
+        WALL_COLOR,
+        meshes,
+        color_materials,
+    );
 
     commands.spawn(factory.get(WallId::Right));
     commands.spawn(factory.get(WallId::Left));
@@ -93,19 +86,41 @@ enum WallId {
 struct WallBundleFactory {
     window_width: f32,
     window_height: f32,
-    wall_thickness: f32,
-    wall_mesh: Handle<Mesh>,
-    wall_material: Handle<ColorMaterial>,
+    thickness: f32,
+    mesh_handle: Handle<Mesh>,
+    material_handle: Handle<ColorMaterial>,
 }
 impl WallBundleFactory {
+    fn new(
+        window_width: f32,
+        window_height: f32,
+        thickness: f32,
+        color: Color,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut color_materials: ResMut<Assets<ColorMaterial>>,
+    ) -> Self {
+        let mesh_handle = meshes.add(Rectangle {
+            half_size: Vec2 {
+                x: thickness / 2.0,
+                y: thickness / 2.0,
+            },
+        });
+
+        let material_handle = color_materials.add(color);
+
+        Self {
+            window_width,
+            window_height,
+            thickness,
+            mesh_handle,
+            material_handle,
+        }
+    }
+
     fn get(&mut self, wall: WallId) -> impl Bundle {
         let collider = match wall {
-            WallId::Upper | WallId::Lower => {
-                Collider::rectangle(self.window_width, self.wall_thickness)
-            }
-            WallId::Left | WallId::Right => {
-                Collider::rectangle(self.wall_thickness, self.window_height)
-            }
+            WallId::Upper | WallId::Lower => Collider::rectangle(self.window_width, self.thickness),
+            WallId::Left | WallId::Right => Collider::rectangle(self.thickness, self.window_height),
         };
 
         let transform = match wall {
@@ -117,17 +132,15 @@ impl WallBundleFactory {
 
         let transform = match wall {
             WallId::Upper | WallId::Lower => {
-                transform.with_scale(Vec3::new(self.window_width / self.wall_thickness, 1.0, 1.0))
+                transform.with_scale(Vec3::new(self.window_width / self.thickness, 1.0, 1.0))
             }
-            WallId::Left | WallId::Right => transform.with_scale(Vec3::new(
-                1.0,
-                self.window_height / self.wall_thickness,
-                1.0,
-            )),
+            WallId::Left | WallId::Right => {
+                transform.with_scale(Vec3::new(1.0, self.window_height / self.thickness, 1.0))
+            }
         };
 
-        let mesh = Mesh2d(self.wall_mesh.clone());
-        let material = MeshMaterial2d(self.wall_material.clone());
+        let mesh = Mesh2d(self.mesh_handle.clone());
+        let material = MeshMaterial2d(self.material_handle.clone());
 
         (mesh, material, collider, transform, RigidBody::Static)
     }
