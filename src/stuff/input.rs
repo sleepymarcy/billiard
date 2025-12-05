436212eb740bba3765e989cd3e14aa59d4b76dd5
sleepymarcy <martyna.sowinska@hotmail.com>
2025-12-05 22:22:46 +0100

    chore: cleanup

--- src/stuff/input.rs
+++
@@ -1,6 +0,0 @@
-use bevy::prelude::*;
-use bevy_enhanced_input::prelude::*;
-
-#[derive(InputAction)]
-#[action_output(Vec2)]
-pub struct Move;
--- src/things/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod lower_white;
-pub mod upper_red;
-
-pub use lower_white::WhiteThing;
-pub use upper_red::RedThing;
