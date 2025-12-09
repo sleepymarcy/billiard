9643f489b7f66b8793a6dcd93f998b8c11cc1fb9
Martyna Marcy Sowińska <61689492+sleepymarcy@users.noreply.github.com>
2025-12-09 20:55:42 +0100

    feat: :construction: add mouse control basis

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,64 @@ fn main() {
                 ball::spawn,
                 // spawn_markers,
                 table::spawn,
+                spawn_mouse_controller,
             ),
         )
-        .add_systems(Update, camera::follow_cue_ball)
+        .add_systems(Update, (camera::follow_cue_ball, get_mouse_delta))
         .insert_resource(SolverConfig {
             restitution_threshold: 0.01,
             ..default()
         })
         .run();
+}
+
+#[derive(Component)]
+struct LmbPast {
+    pub pressed: bool,
+}
+
+#[derive(Component)]
+struct MouseVector {
+    pub beginning: Vec2,
+    pub end: Vec2,
+}
+
+// TODO: improve spawn_mouse_controller
+// 1) use Resource to store intermediate state instead of spawning an entity for this purpose
+fn spawn_mouse_controller(mut commands: Commands) {
+    commands.spawn((
+        LmbPast { pressed: false },
+        MouseVector {
+            beginning: Vec2::ZERO,
+            end: Vec2::ZERO,
+        },
+    ));
+}
+
+// TODO: improve get_mouse_delta
+// 1) instead of Window and cursor position use AccumulatedMouseMotion to get mouse delta
+// 2) use just_pressed instead of pressed - eliminates the need to store past LMB state
+fn get_mouse_delta(
+    mouse_controller: Single<(&mut LmbPast, &mut MouseVector)>,
+    window: Single<&Window>,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+) {
+    let lmb_pressed = mouse_buttons.pressed(MouseButton::Left);
+    mouse_buttons.just_pressed(MouseButton::Left);
+    let (mut lmb_past, mut mouse_vector) = mouse_controller.into_inner();
+    let mouse_position = window.cursor_position();
+
+    if lmb_pressed && !lmb_past.pressed && mouse_position.is_some() {
+        mouse_vector.beginning = mouse_position.unwrap();
+    }
+
+    if !lmb_pressed && lmb_past.pressed && mouse_position.is_some() {
+        mouse_vector.end = mouse_position.unwrap();
+        let mouse_delta = mouse_vector.end - mouse_vector.beginning;
+        println!("{mouse_delta:?}");
+    }
+
+    lmb_past.pressed = lmb_pressed;
 }
 
 #[allow(dead_code)]
